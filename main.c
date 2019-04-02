9aa9a17979dfebb759da52823f7f0ba1f92cefa9
Jonathan Goncalves <jonathan.goncalves@bigpond.com>
2019-04-02 02:59:44 +0000

    converts letters and adds key

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+	#include <stdio.h>
+float Caesar( char c , int key); 
 int main()
 {
-printf("hello");
-//
-return 0:
+    char c; // numerical value for letter
+    int key; // how much c will chift by
+    float result; // numerical value of c after it's been shifted
+    printf("enter value: "); // value for c (symbol)
+    scanf("%c" , &c);
+    printf("enter key value: "); // value for key (how much c will shift by)
+    scanf("%d" , &key);
+    result = Caesar( c , key); 
+    printf("%f\n" , result);  
+}
+float Caesar(char c , int key)
+{
+    float result;
+    c = c - 65;
+    key = key + 65;
+    result = c + key;
+    return result;
 }
